5647854447b6ce3c77a61f2847c41a2604483fca
cristianAnAn <22300509@uttt.edu.mx>
2025-11-25 10:47:17 -0600

    feat(auth, transactions): Validaciones al iniciar sesion y nuevo endpoint de obtener categorias

--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -2,7 +2,7 @@
 import { Router } from 'express';
 import { verifyIdToken } from '../middlewares/auth.js';
 import { createTxValidator, listTxValidator, patchTxValidator, deleteTxValidator } from '../validators/transactions.validators.js';
-import { createTransaction, listTransactions, patchTransaction, deleteTransaction } from '../controllers/transactions.controller.js';
+import { createTransaction, listTransactions, patchTransaction, deleteTransaction, listTransactionCategories } from '../controllers/transactions.controller.js';
 
 const router = Router();
 router.use(verifyIdToken);
@@ -11,5 +11,5 @@ router.post('/', createTxValidator, createTransaction);
 router.get('/', listTxValidator, listTransactions);
 router.patch('/:id', patchTxValidator, patchTransaction);
 router.delete('/:id', deleteTxValidator, deleteTransaction);
-
+router.get('/categories', listTransactionCategories);
 export default router;
